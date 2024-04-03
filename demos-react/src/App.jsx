d2a96cdbc340c1fc01a4e8d4ca9e349c815785b8
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-04-03 14:18:14 +0200

    Ex 5

--- demos-react/src/App.jsx
+++ demos-react/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Clock from './Clock'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showClock, setShowClock] = useState(true);
+  const [delay, setDelay] = useState(1000)
 
   return (
     <>
@@ -28,6 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Delay : <input type="number" value={delay} onChange={(e) => setDelay(e.target.valueAsNumber)} /></p>
+      <button onClick={() => setShowClock(!showClock)}>{showClock ? 'Hide' : 'Show'} Clock</button>
+      {showClock && <Clock delay={delay} />}
     </>
   )
 }
