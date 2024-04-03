441a8215427781b1bc9d037a4337678adab34a64
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-04-03 12:27:50 +0200

    Correction Ex 4

--- todos-react-ts/src/App.tsx
+++ todos-react-ts/src/App.tsx
@@ -1,20 +1,63 @@
+import { FormEvent, MouseEvent, useState } from 'react';
 import './App.css';
 import TodoItem from './TodoItem';
 import { Todo } from './types';
 
 function App() {
-  const todos: Todo[] = [
+  const [todos, setTodos] = useState<Todo[]>([
     { id: 123, title: 'ABC', completed: false },
     { id: 456, title: 'DEF', completed: true },
     { id: 789, title: 'XYZ', completed: false },
-  ];
+  ]);
+  const [newTodo, setNewTodo] = useState('ABC');
+
   const editingId = 789;
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTodos([
+      ...todos,
+      { id: Math.random(), title: newTodo, completed: false },
+    ]);
+    setNewTodo('');
+  }
+
+  function handleToggleClick(event: MouseEvent<HTMLInputElement>) {
+    // peut être utiliser use-immer pour simplifier :
+    // setTodos((draft) => {
+    //   for (const todo of draft) {
+    //     draft.completed = event.target.checked
+    //   }
+    // });
+
+    setTodos(
+      todos.map((todo) => {
+        if (todo.completed === event.currentTarget.checked) {
+          return todo;
+        } else {
+          return {
+            ...todo,
+            completed: event.currentTarget.checked,
+          };
+        }
+      }),
+    );
+  }
 
   return (
     <>
-      <form className="todos-form">
-        <input type="checkbox" className="todos-toggle-checked" />
-        <input type="text" className="todos-new-input" />
+      <form className="todos-form" onSubmit={handleSubmit}>
+        <input
+          type="checkbox"
+          className="todos-toggle-checked"
+          onClick={handleToggleClick}
+        />
+        <input
+          type="text"
+          className="todos-new-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
         <button>+</button>
       </form>
       <div className="todos-container">
